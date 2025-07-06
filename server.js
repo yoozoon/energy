9e7fa98c83b2571cfcf829e00682826e4414180e
yoozoon <yoozoon001@gmail.com>
2025-07-06 15:50:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3455;
 
 // Middleware
 app.use(cors());
